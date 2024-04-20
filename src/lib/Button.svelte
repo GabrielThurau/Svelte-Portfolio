0cf97bd1f65c0cb30762410295c43ad575d33b97
GabrielThurau <gjthurau92@yahoo.com>
2024-04-19 18:35:08 -0700

    first commit

---
+++ src/lib/Button.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let size = "small";
+  export let shadow = false;
+</script>
+
+<button class="size-sm"><slot>Fallback Text</slot></button>
+
+<style lang="scss">
+  button {
+    background: salmon;
+    color: white;
+    &.size-sm {
+      padding: 0.5rem 1rem;
+    }
+    &.size-lg {
+      padding: 1rem 2rem;
+    }
+  }
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let initialCount;
+  export let maxCount = 10;
+
+  let count = initialCount;
+  let string;
+
+  $: {
+    string = `You have clicked ${count} times`;
+  }
+
+  const increment = () => {
+    if (count === maxCount) return;
+    count += 1;
+  };
+</script>
+
+<button class="counter-button" on:click={increment}>
+  count is {count}
+</button>
+
+<h3>{string}</h3>
+
+<style>
+  .counter-button {
+    padding: 15px 20px;
+    background-color: salmon;
+  }
+</style>
